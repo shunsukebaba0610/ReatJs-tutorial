5313fd784162a08911cb650bbf0c96d3ef0ade55
shunsukebaba0610 <51816755+shunsukebaba0610@users.noreply.github.com>
2022-05-31 16:07:08 +0900

    useContext comment add
    useContext comment add

--- react-hooks/src/pages/Context.js
+++ react-hooks/src/pages/Context.js
@@ -4,6 +4,9 @@ import User from "../components/User";
 
 export const AppContext = createContext(null);
 
+/*
+*　createContextで括りつけたコンポネント要素に値を同時に渡す
+*/
 const Context = () => {
   const [username, setUsername] = useState("");
 
